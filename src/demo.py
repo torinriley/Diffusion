611ff210736cbdd12be27c7441b0adb07249c82d
Torin Etheridge <torinriley220@gmail.com>
2024-12-15 11:58:43 -0500

    update

---
+++ src/demo.py
@@ -0,0 +1,50 @@
+import model_loader
+import pipeline
+from PIL import Image
+from pathlib import Path
+from transformers import CLIPTokenizer
+import torch
+
+DEVICE = "cpu"
+
+ALLOW_CUDA = False
+ALLOW_MPS = False
+
+if torch.cuda.is_available() and ALLOW_CUDA:
+    DEVICE = "cuda"
+elif (torch.backends.mps.is_built() or torch.backends.mps.is_available()) and ALLOW_MPS:
+    DEVICE = "mps"
+print(f"Using device: {DEVICE}")
+
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+model_file = "data/v1-5-pruned-emaonly.ckpt"
+models = model_loader.load_models(model_file, DEVICE)
+
+
+prompt = "A painting of Mars from the surface, oil painting style. The sky is red and the ground is orange."
+uncond_prompt = "" 
+do_cfg = True
+cfg_scale = 8 
+
+
+sampler_name = "ddpm"
+num_inference_steps = 50
+seed = 42
+strength = 0.75
+
+output_image = pipeline.generate(
+    prompt=prompt,
+    uncond_prompt=uncond_prompt,
+    do_cfg=do_cfg,
+    cfg_scale=cfg_scale,
+    sampler_name=sampler_name,
+    n_inference_steps=num_inference_steps,
+    seed=seed,
+    strength=strength, 
+    models=models,
+    device=DEVICE,
+    tokenizer=tokenizer
+)
+
+output_image = Image.fromarray(output_image)
+output_image.save("output.png")
